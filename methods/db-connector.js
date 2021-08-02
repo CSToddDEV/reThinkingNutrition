82d4981ac24fbdfa7c91a38ed0f8743a1ad2c90d
Alice Fisher <fisheali@flip1.engr.oregonstate.edu>
2021-08-01 23:47:37 -0700

    added to local db

--- methods/db-connector.js
+++ methods/db-connector.js
@@ -1,14 +1,16 @@
+// ./database/db-connector.js
+
 // Get an instance of mysql we can use in the app
-var mysql = require('mysql');
+var mysql = require('mysql')
 
 // Create a 'connection pool' using the provided credentials
-var pool = mysql.createPool({
+    var pool = mysql.createPool({
     connectionLimit : 10,
     host            : 'classmysql.engr.oregonstate.edu',
-    user            : 'cs340_',
-    password        : '',
-    database        : 'cs340_'
-});
+    user            : 'cs361_fisheali',
+    password        : '8521',
+    database        : 'cs361_fisheali'
+})
 
 // Export it for use in our applicaiton
-module.exports.pool = pool;
+exports.pool = pool; 

9be586dbc3b6a94d7c6a94d12b09763c2e4c1ae0
Alice Fisher <fisheali@flip1.engr.oregonstate.edu>
2021-08-04 15:34:44 -0700

    can add consultation if no duplicate

--- methods/databaseMethods.js
+++ methods/databaseMethods.js
@@ -308,7 +308,9 @@ async function dropDownList(pool, params) {
 
 async function nameRecords(pool, data) {
   //First call to check # of name Records
-  sqlQuery = "SELECT client_id AS 'Client ID', fname AS 'First Name', lname AS 'Last Name', phone AS 'Phone Number', email AS 'Email Address', address AS 'Street Address', city AS 'City'\
+  //"SELECT client_id AS 'Client ID', fname AS 'First Name', lname AS 'Last Name', phone AS 'Phone Number', email AS 'Email Address', address AS 'Street Address', city AS 'City'\
+
+  sqlQuery = "SELECT client_id, fname, lname\
   FROM Clients cl\
   WHERE fname = ?\
     AND lname = ?\

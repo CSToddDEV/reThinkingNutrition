270db46733572e4db9cc759e53569362558fb3fb
Calvin Todd <CSTodd88@gmail.com>
2021-08-04 14:53:12 -0700

    Reverted nameRecords

--- methods/databaseMethods.js
+++ methods/databaseMethods.js
@@ -308,16 +308,14 @@ async function dropDownList(pool, params) {
 
 async function nameRecords(pool, data) {
   //First call to check # of name Records
-  //sqlQuery = "SELECT client_id AS 'Client ID', fname AS 'First Name', lname AS 'Last Name', phone AS 'Phone Number', email AS 'Email Address', address AS 'Street Address', city AS 'City' \
-  sqlQuery = "SELECT client_id, fname, lname\
+  sqlQuery = "SELECT client_id AS 'Client ID', fname AS 'First Name', lname AS 'Last Name', phone AS 'Phone Number', email AS 'Email Address', address AS 'Street Address', city AS 'City' \
   FROM Clients cl\
   WHERE fname = ?\
-    AND lname = ?;"
-    // OR (client_id = ?);";
+    AND lname = ?;\
+    OR (client_id = ?);";
 
   return pool.query(
-    sqlQuery, [data.fname, data.lname]
-    //[data.fname, data.lname, data.client_id]
+    [data.fname, data.lname, data.client_id]
   );
 };
 
